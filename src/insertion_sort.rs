f42fc28db57ae29b90d37fa8c3abc94e402c5477
Brice Maltby <b@bricemaltby.com>
2022-06-01 17:35:25 -0500

    fix array indexing bug on insertion sort

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -1,14 +1,16 @@
 // TODO: should create a 'Sort' trait, each algorithm having it's own 'Impl'
 pub mod insertion_sort {
     pub fn sort(mut a: Vec<isize>, n: usize) -> Vec<isize> {
-        for i in 2..n {
-            let key = a[i];
-            let mut j = i - 1;
-            while j > 0 && a[j] > key {
-                a[j + 1] = a[j];
-                j = j - 1;
+        // [6, 5]
+        for i in 1..n { // 1
+            let key = a[i]; // '5'
+            let mut j = i; // 1
+            while j > 0 && a[j - 1] > key {
+                j-=1; // 0
+                a[j + 1] = a[j]; // [6, 6]
             }
-            a[j + 1] = key;
+            // j = 0, key = '5'
+            a[j] = key; // [5, 6]
         }
         a
     }
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,37 @@ mod tests {
     use super::*;
     #[test]
     fn insertion_sort_sanity() {
-        let input = vec!(1,3,5,2,4);
+        let input = vec!(4,2,5,3,1);
         let len = input.len();
         let output = insertion_sort::insertion_sort::sort(input, len);
         assert_eq!(vec!(1,2,3,4,5), output);
+    }
+    #[test]
+    fn insertion_sort_single() {
+        let input = vec!(1);
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::sort(input, len);
+        assert_eq!(vec!(1), output)
+    }
+    #[test]
+    fn insertion_sort_no_change() {
+        let input = vec!(1,2,3);
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::sort(input, len);
+        assert_eq!(vec!(1,2,3), output)
+    }
+    #[test]
+    fn insertion_sort_negatives() {
+        let input = vec!(1,-2,3);
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::sort(input, len);
+        assert_eq!(vec!(-2,1,3), output)
+    }
+    #[test]
+    fn insertion_sort_blank() {
+        let input = vec!();
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::sort(input, len);
+        assert_eq!(vec!() as Vec<isize>, output)
     }
 }
