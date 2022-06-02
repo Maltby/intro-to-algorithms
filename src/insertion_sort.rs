85c2d089955c555c4f942eef16eec2b3686f7260
Brice Maltby <b@bricemaltby.com>
2022-06-01 21:47:40 -0500

    monotonically decreasing insertion sort

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -1,11 +1,25 @@
 // TODO: should create a 'Sort' trait, each algorithm having it's own 'Impl'
 pub mod insertion_sort {
-    pub fn sort(mut a: Vec<isize>, n: usize) -> Vec<isize> {
+    pub fn monotonically_increasing(mut a: Vec<isize>, n: usize) -> Vec<isize> {
         // [6, 5]
         for i in 1..n { // 1
             let key = a[i]; // '5'
             let mut j = i; // 1
             while j > 0 && a[j - 1] > key {
+                j-=1; // 0
+                a[j + 1] = a[j]; // [6, 6]
+            }
+            // j = 0, key = '5'
+            a[j] = key; // [5, 6]
+        }
+        a
+    }
+    pub fn monotonically_decreasing(mut a: Vec<isize>, n: usize) -> Vec<isize> {
+        // [6, 5]
+        for i in 1..n { // 1
+            let key = a[i]; // '5'
+            let mut j = i; // 1
+            while j > 0 && key >= a[j - 1] { // key = 5, a[j-1] = 6
                 j-=1; // 0
                 a[j + 1] = a[j]; // [6, 6]
             }
--- src/lib.rs
+++ src/lib.rs
@@ -7,35 +7,70 @@ mod tests {
     fn insertion_sort_sanity() {
         let input = vec!(4,2,5,3,1);
         let len = input.len();
-        let output = insertion_sort::insertion_sort::sort(input, len);
+        let output = insertion_sort::insertion_sort::monotonically_increasing(input, len);
         assert_eq!(vec!(1,2,3,4,5), output);
     }
     #[test]
     fn insertion_sort_single() {
         let input = vec!(1);
         let len = input.len();
-        let output = insertion_sort::insertion_sort::sort(input, len);
+        let output = insertion_sort::insertion_sort::monotonically_increasing(input, len);
         assert_eq!(vec!(1), output)
     }
     #[test]
     fn insertion_sort_no_change() {
         let input = vec!(1,2,3);
         let len = input.len();
-        let output = insertion_sort::insertion_sort::sort(input, len);
+        let output = insertion_sort::insertion_sort::monotonically_increasing(input, len);
         assert_eq!(vec!(1,2,3), output)
     }
     #[test]
     fn insertion_sort_negatives() {
         let input = vec!(1,-2,3);
         let len = input.len();
-        let output = insertion_sort::insertion_sort::sort(input, len);
+        let output = insertion_sort::insertion_sort::monotonically_increasing(input, len);
         assert_eq!(vec!(-2,1,3), output)
     }
     #[test]
     fn insertion_sort_blank() {
         let input = vec!();
         let len = input.len();
-        let output = insertion_sort::insertion_sort::sort(input, len);
+        let output = insertion_sort::insertion_sort::monotonically_increasing(input, len);
+        assert_eq!(vec!() as Vec<isize>, output)
+    }
+    #[test]
+    fn insertion_sort_sanity_decreasing() {
+        let input = vec!(4,2,5,3,1);
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::monotonically_decreasing(input, len);
+        assert!(vec!(1,2,3,4,5).iter().rev().eq(output.iter()));
+    }
+    #[test]
+    fn insertion_sort_single_decreasing() {
+        let input = vec!(1);
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::monotonically_decreasing(input, len);
+        assert_eq!(vec!(1), output)
+    }
+    #[test]
+    fn insertion_sort_no_change_decreasing() {
+        let input = vec!(1,2,3);
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::monotonically_decreasing(input, len);
+        assert!(vec!(1,2,3).iter().rev().eq(output.iter()));
+    }
+    #[test]
+    fn insertion_sort_negatives_decreasing() {
+        let input = vec!(1,-2,3);
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::monotonically_decreasing(input, len);
+        assert!(vec!(-2,1,3).iter().rev().eq(output.iter()));
+    }
+    #[test]
+    fn insertion_sort_blank_decreasing() {
+        let input = vec!();
+        let len = input.len();
+        let output = insertion_sort::insertion_sort::monotonically_decreasing(input, len);
         assert_eq!(vec!() as Vec<isize>, output)
     }
 }
